1481d4ef58bcbe0971580b3a05be60a8c6250341
Alianda20 <kelalianda@gmail.com>
2024-06-24 13:03:19 +0300

    Third Commit

--- Home/models.py
+++ Home/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 # Canvas & Bottle Paintings Class
 
 
-class Products(models.Model):
+class OtherImages(models.Model):
+    image = models.ImageField(upload_to='otherImages')
+
+
+class Canvas(models.Model):
     image = models.ImageField(upload_to='prodpics')
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -20,7 +25,21 @@ class Bottles(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     medium = models.CharField(max_length=100)
-    
-    
+
+
 class Gallery(models.Model):
     image = models.ImageField(upload_to='prodpics')
+
+
+class Testimonials(models.Model):
+    image = models.ImageField(upload_to='testimonialpics')
+    testimonial = models.TextField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+
+
+class Artists(models.Model):
+    image = models.ImageField(upload_to='artistpics')
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
